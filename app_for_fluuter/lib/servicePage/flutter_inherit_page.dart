23db0646c77fc3e37b4422b0912a1a49c8d26fe4
Aboout <568644031@qq.com>
2019-07-23 15:26:47 +0800

    ------

--- app_for_fluuter/lib/servicePage/flutter_inherit_page.dart
+++ app_for_fluuter/lib/servicePage/flutter_inherit_page.dart
@@ -6,6 +6,7 @@ class FlutterInheritPage extends Hu {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
+
     return _FlutterInheritPageState();
   }
 }
@@ -27,11 +28,66 @@ class _FlutterInheritPageState extends HuState<FlutterInheritPage> {
       crossAxisAlignment: CrossAxisAlignment.center,
       children: <Widget>[
         Text('header'),
-        Divider()
+        Divider(),
+        RaisedButton(
+          onPressed: () {
+            showEmptyView();
+            Future.delayed(Duration(seconds: 3)).then((value){
+              hiddenEmptyView();
+            });
+          },
+          child: Text('showEmptyView'),
+
+        ),
+        RaisedButton(
+          onPressed: () {
+            hiddenEmptyView();
+          },
+          child: Text('hiddenEmptyView'),
+
+        ),
+        RaisedButton(
+          onPressed: () {
+            showLoadingEmptyView();
+            Future.delayed(Duration(seconds: 3)).then((value){
+              hiddenEmptyView();
+            });
+          },
+          child: Text('showEmtyViewLoading'),
+        ),
       ],
     );
   }
 
+}
+
+
+class SecondPage extends Hu {
+
+  @override
+  State<StatefulWidget> createState() {
+    return _SecondPageState();
+  }
+}
+
+class _SecondPageState extends HuState<SecondPage> {
+
+  @override
+  String get title => 'SecondPage';
+
+  @override
+  Widget buildContent(BuildContext context) {
+    return Center(
+      child: Text('SecondPage'),
+    );
+  }
+}
+
+//---------------------------------------------------------------------------
+enum HuEmptyViewState {
+  Loading,
+  hidden,
+  show,
 }
 
 
@@ -44,11 +100,10 @@ abstract class Hu extends StatefulWidget {
 abstract class HuState<T extends Hu> extends State<T> with EmptyState {
 
   String title = "";
+  HuEmptyViewState _emptyViewState = HuEmptyViewState.hidden;
 
 
   Widget buildContent(BuildContext context);
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -57,24 +112,64 @@ abstract class HuState<T extends Hu> extends State<T> with EmptyState {
       appBar: AppBar(
         title: Text(title),
       ),
-      body: buildContent(context),
+      body: _bodyWidget()
     );
+  }
 
+
+  // ignore: slash_for_doc_comments
+  /***************************** empty view start *************************************/
+  @override
+  void showLoadingEmptyView() {
+
+    setState(() {
+      _emptyViewState = HuEmptyViewState.Loading;
+    });
   }
 
   @override
   void showEmptyView() {
-
+    setState(() {
+      _emptyViewState = HuEmptyViewState.show;
+    });
   }
+
+  @override
+  void hiddenEmptyView() {
+    setState(() {
+      _emptyViewState = HuEmptyViewState.hidden;
+    });
+  }
+  // ignore: slash_for_doc_comments
+  /***************************** empty view end *************************************/
+
+
+  Widget _bodyWidget() {
+    switch(_emptyViewState) {
+      case HuEmptyViewState.Loading:
+        return Center(child: CircularProgressIndicator(),);
+      case HuEmptyViewState.show:
+        return Container(color: Colors.purple,);
+      case HuEmptyViewState.hidden:
+        return buildContent(context);
+      default:
+        break;
+    }
+  }
+
 }
 
 
 mixin EmptyState<T extends StatefulWidget> on State<T> {
 
+  void showLoadingEmptyView();
   void showEmptyView();
-
+  void hiddenEmptyView();
 }
 
+abstract class ProgressState<T extends StatefulWidget> extends State<T> {
 
+  void showProgressHud();
+}
 
 
